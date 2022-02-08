61730b2e5c0307c3314a6e0118dbf7eeeb2d7919
seungchanHan <seungchanhan00@gmail.com>
2022-02-09 01:03:13 +0900

    type basic

--- class-note/1_type_basic.ts
+++ class-note/1_type_basic.ts
@@ -1,22 +1,22 @@
 //JS 문자열 선언
-let string = 'hello'
+let string = 'hi'
 
 // TS 문자열 선언
-let str : string = 'hello';
+let str: string = 'hello';
 // TS 숫자 선언
-let num : number = 10
+let num: number = 10
 // TS 배열 선언
-let arr : Array<number> = [1,2,3];
-let hero : Array<string> = ['Capt', 'Thor', 'Hulk'];
-let items : number[] = [1,2,3];
+let arr: Array<number> = [1, 2, 3];
+let hero: Array<string> = ['Capt', 'Thor', 'Hulk'];
+let items: number[] = [1, 2, 3];
 //TS 튜플
-let company : [string, number] = ['GangNam', 100]
+let company: [string, number] = ['GangNam', 100]
 //TS 객체
-let obj : object = {}
+let obj: object = {}
 
-let person : {name :string, age :number} = {
-  name : 'khan',
-  age : 23
+let person: { name: string, age: number } = {
+  name: 'khan',
+  age: 23
 }
 //TS 진위값
-let show :boolean = true
+let show: boolean = true
