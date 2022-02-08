bfe916982545ad4d8bbc30d1e586a23f2a3aea1b
seungchan Han <93566353+Han-Seung-Chan@users.noreply.github.com>
2022-02-09 01:17:25 +0900

    Delete getting-starting directory

--- getting-starting/index.js
+++
@@ -1,4 +0,0 @@
-function subtraction(a, b) {
-    return a - b;
-}
-subtraction(20, 5);
--- getting-starting/index.ts
+++
@@ -1,5 +0,0 @@
-function subtractionTS (a:number, b:number) :number{
-  return a-b
-}
-
-subtractionTS(20, 5)
